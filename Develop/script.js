099edc1befd4399fe6173e13e44d4f531e0a5014
maribels_are_ringing <maribel.duron86@gmail.com>
2021-03-05 19:03:30 -0700

    resturcted my files/folders

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+var ContainerEl = document.querySelector("#container");
+var currentDay = document.querySelector("#currentDay");
+
+var currentDay = moment().format('LLLL');
+console.log(currentDay);
