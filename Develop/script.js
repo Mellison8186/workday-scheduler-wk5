9d9968a89c157bf1be31cdd8064f772d16194c35
maribels_are_ringing <maribel.duron86@gmail.com>
2021-03-07 20:10:56 -0700

    added start of color code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
-var ContainerEl = document.querySelector("#container");
-var currentDay = document.querySelector("#currentDay");
+var ContainerEl = document.querySelector('#container');
+var currentDay = document.querySelector('#currentDay');
+var currentTime = moment().format('LLLL');
+$('#currentDay').html(currentTime);
 
-var currentDay = moment().format('LLLL');
-console.log(currentDay);
+// function to save/set event description in local storage
+$('.saveBtn').on('click', function(){
+    var description = $(this).parent().siblings('td').children().val();
+    var hour = parseInt($(this).parent().siblings('td').children().attr('id'));
+    console.log(hour);
+    window.localStorage.setItem(hour, description);
+});
+
+// retrieves the event from local storage
+$('#8').val(window.localStorage.getItem('8'));
+console.log(window.localStorage.getItem('8'));
+
+// function to check event time
+var checkTime = function() {
+    $('input').each(function() {
+        var timeNow = parseInt(moment().format('HH'));
+        var eventHour = parseInt($(this).attr('id'));
+    if(eventHour < timeNow) {
+        $(this).addClass('past');
+    }
+    if (eventHour === timeNow) {
+        $(this).addClass('present');
+    }
+    })
+};
+checkTime();
